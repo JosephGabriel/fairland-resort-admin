79e4a0fc86ef93d3bb2dc74b0b47a29804baa2d3
JosephGabriel <josephgabriel.jgsm@gmail.com>
2023-08-13 16:10:30 -0300

    dotenv adicionado ao codegen

--- codegen.ts
+++ codegen.ts
@@ -1,4 +1,9 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
+import dotenv from "dotenv";
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
 
 const scalars = {
   EmailAddress: "string",
@@ -12,7 +17,7 @@ const scalars = {
 const config: CodegenConfig = {
   overwrite: true,
   watch: true,
-  schema: "https://fairland-resort-api-c5b3bb10838f.herokuapp.com/graphql",
+  schema: process.env.VITE_BASE_URL,
   documents: "src/**/*.graphql",
   generates: {
     "src/services/apollo/hooks.ts": {
@@ -26,7 +31,6 @@ const config: CodegenConfig = {
         withHooks: true,
         reactApolloVersion: 3,
         strictScalars: true,
-        avoidOptionals: true,
         scalars,
       },
     },
